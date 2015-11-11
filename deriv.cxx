2c73a5df71c638cdbc73b5e64c48d6d208c5d83d
Jacqueline Fedyk <jafed100@pc07.pool.tp1.uni-duesseldorf.de>
2015-11-11 17:26:13 +0100

    lab4bearbeitet

--- deriv.cxx
+++ deriv.cxx
@@ -7,8 +7,29 @@ using namespace std;
 void print(double* p, const int N, const double dx,
            const double xmin)
 {
-   for(int i=0; i<N; i++)
-       cout << i*dx + xmin << "\t" << p[i] << endl;
+   for(int i=0; i<N; i++){    
+     cout << i*dx + xmin << "\t" << p[i] << endl;
+   }
+}
+
+void Wolf(double* p, const int N, const double dx,
+           const double xmin)
+{
+    for(int i=0; i<N; i++){
+        p[i]=exp(-(i*dx + xmin)*(i*dx + xmin));
+    }
+}
+void henry(double* p, const int N, const double dx,
+           const double xmin)
+{
+  double p1,p0=0;
+  
+  for(int i=0; i<N-1; i++){
+    p1=p[i];
+    p[i]=(p[i+1]-p0)/(2*dx);
+    p0=p1; 
+  }
+  p[N-1]=p0/(2*dx);
 }
 
 
@@ -18,9 +39,13 @@ int main(){
   const double xmax = 15;
   const double xmin = -15;
   const double dx = (xmax-xmin)/(N-1);
+  
 
   // call to function which fills array p here
   // call to functio which calculates the derivative
+
+  Wolf(p,N,dx,xmin);
+  henry(p,N,dx,xmin);
   print(p,N,dx,xmin);
 
   return 0;
